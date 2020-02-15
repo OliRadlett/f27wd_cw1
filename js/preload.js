95d773ef32212aafacd87f77a8483a494d9249d3
Oli Radlett <o.radlett@gmail.com>
2020-02-15 19:58:06 +0000

    Add cards to homepage and fix animation and page height

--- js/preload.js
+++ js/preload.js
@@ -1,5 +1,12 @@
-window.onload = () => {
-    
-    document.getElementsByClassName("preload")[0].classList.remove("preload");
+/*
 
-}
+    Script to delay CSS animations by 500ms to stop expand animation runing on page load    
+
+*/
+
+setTimeout(function() {
+
+    // Remove preload class from body to allow animations to play again
+    document.body.className = "";
+
+}, 500);
